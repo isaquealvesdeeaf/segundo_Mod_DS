861fe7c128ff5740aafb258dbb56a3e25e48fb44
isaquealvesdeeaf <isaquealves5544@gmail.com>
2019-08-22 20:28:15 -0300

    Atualização do EX4 , 5

---
+++ aula7/exemplo4/Exemplo5/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exemplo5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //instanciando a variavel Stream
+            FileStream stream = new FileStream(@"Exemplo4.bin", FileMode.Open);
+
+            //instanciando a variavel do tipo BinaryReader
+            BinaryReader reader = new BinaryReader(stream);
+
+            //variaveis recebendo os valores lidos pelo arquivo binario
+            int idade = reader.ReadInt32();
+            string nome = reader.ReadString();
+            bool temFilhos = reader.ReadBoolean();
+            double altura = reader.ReadDouble();
+
+            //Fecha o acesso com o arquivo
+            reader.Close();
+        }
+    }
+}
---
+++ aula7/exemplo4/exemplo4/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exemplo4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //instanciando as variaveis
+            int idade = 25;
+            string nome = "Sérgio";
+            bool temFilho = false;
+            double altura = 1.79;
+
+
+            //Criando arquivo
+            FileStream stream = new FileStream(@"C:\Teste\Exemplo4.bin", FileMode.Create);
+
+            //instanciando a variavel do tipo BinaryWriter
+            BinaryWriter writer = new BinaryWriter(stream);
+
+            //Usando o método Write para escrever no arquivo.
+            writer.Write(idade);
+            writer.Write(nome);
+            writer.Write(temFilho);
+            writer.Write(altura);
+
+            writer.Flush();
+            writer.Close();
+        }
+    }
+}
